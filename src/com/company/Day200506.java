17c98117305c12185a7bd106123b369a88135742
Lionhead93 <lionhead115@naver.com>
2020-05-06 15:55:49 +0900

    Merge Trees

--- src/com/company/Day200506.java
+++ src/com/company/Day200506.java
@@ -39,4 +39,34 @@ public class Day200506 {
         return list;
     }
 
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1 == null) {
+            return t2;
+        }
+        if(t2 == null) {
+            return t1;
+        }
+
+        return addNodes(t1, t2);
+    }
+
+    private TreeNode addNodes(TreeNode t1, TreeNode t2) {
+
+        if(t1 == null && t2 == null) {
+            return null;
+        }
+
+        if(t1 == null) {
+            return t2;
+        } else if(t2 == null) {
+            return t1;
+        }
+
+        TreeNode tn = new TreeNode(t1.val + t2.val);
+        tn.left = addNodes(t1.left, t2.left);
+        tn.right = addNodes(t1.right, t2.right);
+
+        return tn;
+    }
+
 }
--- src/com/company/TreeNode.java
+++ src/com/company/TreeNode.java
@@ -5,6 +5,11 @@ public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
+    TreeNode() {}
     TreeNode(int x) {val = x;}
-
+    TreeNode(int x, TreeNode left, TreeNode right) {
+        this.val = x;
+        this.left = left;
+        this.right = right;
+    }
 }
