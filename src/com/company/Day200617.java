3dbeef913c1879eec922471895e36b61240ed70a
Lionhead93 <lionhead115@naver.com>
2020-06-17 11:22:15 +0900

    no message

--- src/com/company/Day200617.java
+++ src/com/company/Day200617.java
@@ -1,4 +1,35 @@
 package com.company;
 
 public class Day200617 {
+
+    /**
+     * @author seongwou
+     * @since 2020/06/17 11:18 오전
+     *
+     * You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+     * Find out how many ways to assign symbols to make sum of integers equal to target S.
+     *
+     * Example 1:
+     * Input: nums is [1, 1, 1, 1, 1], S is 3.
+     * Output: 5
+     * Explanation:
+     * -1+1+1+1+1 = 3
+     * +1-1+1+1+1 = 3
+     * +1+1-1+1+1 = 3
+     * +1+1+1-1+1 = 3
+     * +1+1+1+1-1 = 3
+     *
+     * There are 5 ways to assign symbols to make the sum of nums be target 3.
+     *
+     * Constraints:
+     * The length of the given array is positive and will not exceed 20.
+     * The sum of elements in the given array will not exceed 1000.
+     * Your output answer is guaranteed to be fitted in a 32-bit integer.
+     **/
+    public int findTargetSumWays(int[] nums, int S) {
+        int answer = 0;
+        
+        return answer;
+    }
+
 }
