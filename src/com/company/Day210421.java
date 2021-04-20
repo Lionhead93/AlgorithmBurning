bd3e57bf7ae0a3f80ff7a8cd351c142b834c9d20
Lionhead93 <lionhead115@naver.com>
2021-04-20 22:18:36 +0900

    210420

---
+++ src/com/company/Day210421.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Day210421 {
+
+    static void minimumBribes(int[] q) {
+        for(int i = 0 ; i < q.length ; i ++) {
+            if(q[i]-(i+1)>2) {
+                System.out.println("Too chaotic");
+                return;
+            }
+        }
+        int count = 0;
+        for(int i = 0 ; i < q.length ; i ++) {
+            if(i+1 != q[i]) {
+                int tmpS = i+1;
+                for(int j = i+1; j< q.length ; j++) {
+                    if(q[j] == tmpS) {
+                        int tmp = q[j-1];
+                        q[j-1] = q[j];
+                        q[j] = tmp;
+
+                        count += 1;
+                        i--;
+                        j = q.length;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+}
