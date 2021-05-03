9ba3f4957d2d33cce22b86c74973b2900fdbe76a
Lionhead93 <lionhead115@naver.com>
2021-05-03 22:06:40 +0900

    merge sort

---
+++ src/com/company/Day210504.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Day210504 {
+
+    public static void mergeSort(int[] arr) {
+        sort(arr, 0, arr.length);
+    }
+
+    private static void sort(int[] arr, int low, int high) {
+        if (high - low < 2) {
+            return;
+        }
+
+        int mid = (low + high) / 2;
+        sort(arr, 0, mid);
+        sort(arr, mid, high);
+        merge(arr, low, mid, high);
+    }
+
+    private static void merge(int[] arr, int low, int mid, int high) {
+        int[] temp = new int[high - low];
+        int t = 0, l = low, h = mid;
+
+        while (l < mid && h < high) {
+            if (arr[l] < arr[h]) {
+                temp[t++] = arr[l++];
+            } else {
+                temp[t++] = arr[h++];
+            }
+        }
+
+        while (l < mid) {
+            temp[t++] = arr[l++];
+        }
+
+        while (h < high) {
+            temp[t++] = arr[h++];
+        }
+
+        for (int i = low; i < high; i++) {
+            arr[i] = temp[i - low];
+        }
+    }
+
+
+}
