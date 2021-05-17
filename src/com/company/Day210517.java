dc6c88772a238d78e61289afc245c25adea917b5
Lionhead93 <lionhead115@naver.com>
2021-05-17 21:46:28 +0900

    funding price

---
+++ src/com/company/Day210517.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Day210517 {
+    public int[] solution(int[] prices) {
+        Stack<Integer> beginIdxs = new Stack<>();
+        int i=0;
+        int[] terms = new int[prices.length];
+
+        beginIdxs.push(i);
+        for (i=1; i<prices.length; i++) {
+            while (!beginIdxs.empty() && prices[i] < prices[beginIdxs.peek()]) {
+                int beginIdx = beginIdxs.pop();
+                terms[beginIdx] = i - beginIdx;
+            }
+            beginIdxs.push(i);
+        }
+        while (!beginIdxs.empty()) {
+            int beginIdx = beginIdxs.pop();
+            terms[beginIdx] = i - beginIdx - 1;
+        }
+
+        return terms;
+    }
+}
