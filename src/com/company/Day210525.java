00afeff8669be9586fe1c95baf2fbbfb7f9b4ebc
Lionhead93 <lionhead115@naver.com>
2021-05-25 16:48:48 +0900

    robber

---
+++ src/com/company/Day210525.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Day210525 {
+
+    /**
+     * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed,
+     * the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected
+     * and it will automatically contact the police if two adjacent houses were broken into on the same night.
+     * <p>
+     * Given an integer array nums representing the amount of money of each house,
+     * return the maximum amount of money you can rob tonight without alerting the police.
+     *
+     * @param nums
+     * @return
+     */
+    public int rob(int[] nums) {
+        return 0;
+    }
+
+}
